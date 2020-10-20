1a301950d4435a1090eee922dbd3d7dae94381c3
Amir Masoud Jalali <amir02jalali@gmail.com>
2020-10-20 15:38:38 +0330

    simply add an text

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ export default function App() {
     <div>
       <p>
         {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be{' '}
-        <strong>{results.length}</strong> flying horses
+        <strong>{results.length}</strong> flying horses & monsters
       </p>
       <hr></hr>
       {results
